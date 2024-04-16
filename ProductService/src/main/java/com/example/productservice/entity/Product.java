10451bc60e7ee6d2db6aec9a3e08772d3b9772f8
Ryan Shum <ryancks09@gmail.com>
2024-04-15 22:48:11 -0400

    Fixed Flyway script location

--- ProductService/src/main/java/com/example/productservice/entity/Product.java
+++ ProductService/src/main/java/com/example/productservice/entity/Product.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
