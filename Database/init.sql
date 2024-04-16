f6c43f979e10218ad2d9460dfb4d9c9ba376b1c5
gemrobino <gemrobino@gmail.com>
2024-04-16 04:08:58 -0400

    Update docker configurations

---
+++ Database/init.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS assignment2;
+USE assignment2;
+
+CREATE TABLE product
+(
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    description TEXT,
+    price       DECIMAL(10, 2) NOT NULL,
+    stock       INT NOT NULL,
+    image_id    BIGINT NOT NULL
+);
