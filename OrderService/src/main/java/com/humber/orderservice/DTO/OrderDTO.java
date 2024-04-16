494246b17f58a649322ccb385b13d37448592e4d
Angelique Ulep <ulepangelique@gmail.com>
2024-04-15 23:09:00 -0400

    Implement Order Microservice

---
+++ OrderService/src/main/java/com/humber/orderservice/DTO/OrderDTO.java
@@ -0,0 +1,18 @@
+package com.humber.orderservice.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDTO {
+    private Long id;
+    private LocalDateTime orderDate;
+    private double totalPrice;
+    private List<OrderItemDTO> orderItems;
+}
---
+++ OrderService/src/main/java/com/humber/orderservice/DTO/ProductDTO.java
@@ -0,0 +1,18 @@
+package com.humber.orderservice.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private int stock;
+    private Long imageId;
+}
